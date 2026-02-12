74f24fb3c8d2c7e5c4efe6ddec5f8d12f07b16d4
shalom <15121177287@163.com>
2026-02-12 22:59:01 +0800

    mv vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,12 @@
-// 针对 ?raw 后缀的声明
+/// <reference types="vite/client" />
+
 declare module '*?raw' {
     const content: string;
     export default content;
-}
-
-// 针对 ?inline 后缀的声明（如果你用的是 inline）
-declare module '*?inline' {
+  }
+  
+  declare module '*?inline' {
     const content: string;
     export default content;
-}
+  }
+  
